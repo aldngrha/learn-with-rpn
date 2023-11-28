484b3acea599b07067dc197ddc2651b3062302e4
Aldi Nugraha <aldi.nugrahatk@gmail.com>
2023-11-28 10:38:04 +0700

    first commit

---
+++ basic.js
@@ -0,0 +1,44 @@
+let a = "a";
+const b = "b";
+var c = "c";
+
+let name = "Aldi";
+let age = 25;
+let isTrue = true;
+let money = 5000.5;
+
+
+let number1 = 10;
+let number2 = 20;
+console.log(number1 + number2);
+console.log(number1 - number2);
+console.log(number1 / number2);
+console.log(number1 * number2);
+console.log(number1 % number2);
+
+let result = number1 + number2;
+console.log(result);
+penampungHasil = number1 - number2;
+console.log(result);
+penampungHasil = number1 / number2;
+console.log(penampungHasil);
+penampungHasil = number2 * number2;
+console.log(penampungHasil);
+penampungHasil = number2 % number2;
+console.log(penampungHasil);
+
+let firstName = "Aldi";
+let lastName = "Nugraha";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+console.log(firstName + " " + lastName);
+
+
+let number = 20;
+console.log(number + 10); //30
+number = 10;
+console.log(number + 10); //20
+
+const animal = "gajah";
+console.log(animal);
+animal = "kucing";
